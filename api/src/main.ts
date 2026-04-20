ffe22341d75292c1ea1054ed3465f87c80e5e828
Ford-Kornthiwa <kornthiwa@gmail.com>
2026-04-20 17:34:33 +0700

    open cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
